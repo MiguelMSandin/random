c43d13c281f6c79b8690de8db3b67b499ca7a5ce
MiguelMSandin <miguelmendezsandin@gmail.com>
2021-02-01 18:28:05 +0100

    Add files via upload

---
+++ scripts/multi2linefasta.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import argparse
+from Bio import SeqIO
+
+parser = argparse.ArgumentParser(description="Converts a fasta file where the sequences are in multiple lines to a fasta file where each sequence is in one line.")
+
+parser.add_argument("-f", "--file", dest="file_in", required=True,
+                    help="Input fasta file.")
+
+parser.add_argument("-o", "--output", dest="file_out", required=True,
+                    help="Output fasta file.")
+
+args = parser.parse_args()
+
+with open(args.file_out, "w") as outfile:
+    for line in SeqIO.parse(open(args.file_in), "fasta"):
+        print(">" + line.id + "\n" + line.seq, file=outfile)
+            
