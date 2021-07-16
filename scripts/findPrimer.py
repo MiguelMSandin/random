983d4389638edb2d542f133b61384e3ec77340a1
MiguelMSandin <miguelmendezsandin@gmail.com>
2021-07-16 22:30:55 +0200

    Update findPrimer.py

--- scripts/findPrimer.py
+++ scripts/findPrimer.py
@@ -3,17 +3,17 @@
 import argparse
 from Bio import SeqIO
 
-parser = argparse.ArgumentParser(description="From a targeted fasta file, select all possible primers of length 'l', that matches 'm' sequences and has a maximum specificity 's' to the reference database.")
+parser = argparse.ArgumentParser(description="From a targeted fasta file (t/target), find all possible specific primers/probes of length 'l', that matches at least 'm%' of the sequences (or 'M' sequences) in the targeted file and has a maximum specificity of 's%' of the sequences (or 'S' sequences) to the reference (r/reference) database.")
 
 # Add the arguments to the parser
-parser.add_argument("-t", "--target", dest="target", required=True,
+parser.add_argument("-t", "--target", required=True,
 					help="A fasta file with the sequences you want to find a primer to match.")
 
 parser.add_argument("-r", "--reference", dest="reference", required=True,
 					help="A reference file to look against. The targeted group shouldn't be included in the reference. If you are not sure whether it is included or not use the option '-s/--specificity'.")
 
 parser.add_argument("-o", "--output", dest="output", required=True,
-					help="The name of the output fasta and log files. Please note the extensions '.fasta' and '.tsv' will be added to the specify name respectively.")
+					help="The name of the output fasta and log files. Please note the extensions '.fasta' and '.tsv' will be added to the specify name respectively ('output.fasta' and 'output.tsv').")
 
 parser.add_argument("-l", "--length", dest="length", required=False, action='store', default='18+22',
 					help="The desire length of the primers to be searched. Several lengths can be selected by specifying a range with a '+' sign in between. By default it will look for primers of length 18, 19, 20, 21 and 22 base pairs ('18+22').")
@@ -29,6 +29,12 @@ parser.add_argument("-m", "--minimum", dest="minimum", required=False, action='s
 
 parser.add_argument("-M", "--minimumAbs", dest="minimumAbs", required=False, action='store', type=int, default=None,
 					help="Same as '-m/--minimum' but absolute values.")
+
+parser.add_argument("-p", "--probe", dest="probe", required=False, default=None, action="store_true",
+					help="If selected, will also return the reverse complement of the primer found to be used for reverse primers or probes in the logfile.")
+
+parser.add_argument("-b", "--best", dest="best", required=False, default=None, action="store_true",
+					help="If selected, will search for the best scoring primers and output the results to 'OUTPUT_best.tsv'.")
 
 parser.add_argument("-v", "--verbose", dest="verbose", required=False, default=None, action="store_true",
 					help="If selected, will print information to the console.")
@@ -47,36 +53,49 @@ if '+' in args.length:
 	ma = args.length.split('+')[1]
 	lengths = range(int(mi), int(ma)+1)
 else:
-	lengths = int(args.lengths)
+	lengths = int(args.length)
 if verbose:
 	print("    Lengths:    ", *lengths)
 
 # Setting the specificities ________________________________________________________________________
 if args.specificityAbs is not None:
 	specificity = args.specificityAbs
+	ext = "sequences"
 elif args.specificity != 0.01:
 	specificity = args.specificity
+	ext = "%"
+	if args.specificityAbs is not None:
+		print("    Warning!! '-s/--specificity' has been parsed by '-S/--specificityAbs'. Taking absolute values.")
 else:
 	specificity = 0.01
+	ext = "%"
 	if args.specificityAbs is not None:
 		print("    Warning!! '-s/--specificity' has been parsed by '-S/--specificityAbs'. Taking absolute values.")
 if verbose:
-	print("    Specificity:", specificity)
+	print("    Specificity:", specificity, ext)
 
 if args.minimumAbs is not None:
 	minimum = args.minimumAbs
+	ext = "sequences"
 elif args.minimum != 0.8:
 	minimum = args.minimum
+	ext = "%"
 else:
 	minimum = 0.8
+	ext = "%"
 	if args.minimumAbs is not None:
 		print("    Warning!! '-m/--minimum' has been parsed by '-M/--minimumAbs'. Taking absolute values.")
 if verbose:
-	print("    Minimum:    ", minimum)
+	print("    Minimum:    ", minimum, ext)
+
+if args.probe is not None:
+	from Bio.Seq import Seq
+	if verbose:
+		print("    Reverse-complement option selected (-p/--probe)")
 
 # Reading target file ______________________________________________________________________________
 if verbose:
-	print("  Reading target file...")
+	print("  Reading target file...", end="")
 
 target = {}
 w1 = 0
@@ -86,13 +105,13 @@ for line in SeqIO.parse(open(args.target), "fasta"):
 		w1 += 1
 
 if verbose:
-	print("    Sequences in target file:   ", len(target))
+	print("\r    Sequences in target file ('", str(args.target), "'):   ", len(target), sep="")
 	if w1 > 0:
 		print("    Warning!!", args.target, "contains gaps")
 
 # Reading reference file ___________________________________________________________________________
 if verbose:
-	print("  Reading reference file...")
+	print("  Reading reference file...", end="")
 
 ref = {}
 w2 = 0
@@ -102,7 +121,7 @@ for line in SeqIO.parse(open(args.reference), "fasta"):
 		w2 += 1
 
 if verbose:
-	print("    Sequences in reference file:", len(ref))
+	print("\r    Sequences in reference file ('", str(args.reference), "'):   ", len(ref), sep="")
 	if w2 > 0:
 		print("    Warning!!", args.reference, "contains gaps")
 
@@ -111,7 +130,10 @@ primers = set()
 logFile = str(args.output + ".tsv")
 fasFile = str(args.output + ".fasta")
 with open(logFile, "w") as logfile, open(fasFile, "w") as fasfile:
-	print("identifier\tsequence\tmatch_ref\tmatch_ref_abs\tmatch_target\tmatch_target_abs\t\t", file=logfile)
+	if args.probe is not None:
+		print("identifier\tlength\tsequence\tsequence_revCom\tmatch_ref\tmatch_ref_abs\tmatch_target\tmatch_target_abs", file=logfile)
+	else:
+		print("identifier\tlength\tsequence\tmatch_ref\tmatch_ref_abs\tmatch_target\tmatch_target_abs", file=logfile)
 	for length in lengths:  # Loop through the different lengths
 		if verbose:
 			print("  Searching primers of", str(length), "base pairs...")
@@ -119,7 +141,7 @@ with open(logFile, "w") as logfile, open(fasFile, "w") as fasfile:
 		for tv in target.values():  # Loop through all the sequences in the target file
 			if verbose:
 				i += 1
-				print("\r    ", str(round(i/len(target)*100, 1)), "%", sep="", end="")
+				print("\r    ", str(i), "/", len(target), sep="", end="")
 				if i == len(target):
 					print("\r    Completed")
 			l = len(tv)
@@ -127,6 +149,7 @@ with open(logFile, "w") as logfile, open(fasFile, "w") as fasfile:
 				pprimer = tv[int(p):(int(p+length))]
 				if pprimer not in primers:  # Check if the potential primer is already in the list
 					r = 0
+					c = 0
 					for toCheck in target.values():  # Check how many times the potential primer is repeated in the target file
 						if pprimer in toCheck:
 							r += 1
@@ -135,7 +158,6 @@ with open(logFile, "w") as logfile, open(fasFile, "w") as fasfile:
 					else:  # Check if the potential primer is repeated more than 'm' times
 						R = r/len(target)
 					if R >= minimum:  # Check if the potential primer matches the requirements against the target file
-						c = 0
 						for toCheck in ref.values():  # Check how many times the potential primer is repeated in the reference file
 							if pprimer in toCheck:
 								c += 1
@@ -143,9 +165,25 @@ with open(logFile, "w") as logfile, open(fasFile, "w") as fasfile:
 						C = c
 					else:  # Check if the potential primer is repeated less than 's' times
 						C = c/len(ref)
-					if C <= specificity:  # Check if the potential primer matches the requirements against the reference file
+					if C <= specificity and R >= minimum:  # Check if the potential primer matches the requirements 
 							primers.add(pprimer)
-							print("primer" + str(len(primers)) + "\t" + str(pprimer) + "\t" + str(c/len(ref)) + "\t" + str(c) + "\t" + str(r/len(target)) + "\t" + str(r), file=logfile)
-							print(">primer" + str(len(primers)) + "\n" + pprimer, file=fasfile)
+							if args.probe is not None:
+								print("primer" + str(len(primers)) + "\t" + str(len(pprimer)) + "\t" + str(pprimer) + "\t" + str(Seq(pprimer).reverse_complement()) + "\t" + str(c/len(ref)) + "\t" + str(c) + "\t" + str(r/len(target)) + "\t" + str(r), file=logfile)
+								print(">primer" + str(len(primers)) + "\n" + pprimer, file=fasfile)
+							else:
+								print("primer" + str(len(primers)) + "\t" + str(pprimer) + "\t" + str(c/len(ref)) + "\t" + str(c) + "\t" + str(r/len(target)) + "\t" + str(r), file=logfile)
+								print(">primer" + str(len(primers)) + "\n" + pprimer, file=fasfile)
 
+if args.best is not None:
+	if verbose:
+		print("  Searching best scoring primers...")
+	import pandas as pd
+	log = pd.read_csv(logFile, sep="\t")
+	match_ref = min(log["match_ref"])
+	match_target = max(log["match_target"])
+	best = log[(log["match_ref"] == match_ref) & (log["match_target"] == match_target)]
+	bestFile = str(args.output + "_best.tsv")
+	best.to_csv(bestFile, sep="\t")
 
+if verbose:
+	print("Done")
