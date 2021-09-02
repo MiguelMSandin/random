eb7f456a96a341638a82ca6c9b083b0644eb9963
MiguelMSandin <miguelmendezsandin@gmail.com>
2021-09-02 15:20:59 +0200

    Update multi2linefasta.py

--- scripts/multi2linefasta.py
+++ scripts/multi2linefasta.py
@@ -5,10 +5,12 @@ from Bio import SeqIO
 
 parser = argparse.ArgumentParser(description="Converts a fasta file where the sequences are in multiple lines to a fasta file where each sequence is in one line.")
 
-parser.add_argument("-f", "--file", dest="file_in", required=True,
+requiredArgs = parser.add_argument_group('required arguments')
+
+requiredArgs.add_argument("-f", "--file", dest="file_in", required=True,
                     help="Input fasta file.")
 
-parser.add_argument("-o", "--output", dest="file_out", required=True,
+requiredArgs.add_argument("-o", "--output", dest="file_out", required=True,
                     help="Output fasta file.")
 
 args = parser.parse_args()
@@ -16,4 +18,4 @@ args = parser.parse_args()
 with open(args.file_out, "w") as outfile:
     for line in SeqIO.parse(open(args.file_in), "fasta"):
         print(">" + line.id + "\n" + line.seq, file=outfile)
-            
+
