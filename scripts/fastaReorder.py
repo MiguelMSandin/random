da2fc20daec8f33fd094af8f2f9d9fceb2bfbad2
MiguelMSandin <miguelmendezsandin@gmail.com>
2021-09-02 15:18:04 +0200

    Update fastaReorder.py

--- scripts/fastaReorder.py
+++ scripts/fastaReorder.py
@@ -6,38 +6,40 @@ from Bio import SeqIO, Phylo
 parser = argparse.ArgumentParser(description="Reorders a fasta file based on a rooted tree.")
 
 # Add the arguments to the parser
-parser.add_argument("-f", "--fasta", dest="file_in", required=True,
-                    help="Fasta file.")
+requiredArgs = parser.add_argument_group('required arguments')
 
-parser.add_argument("-t", "--tree", dest="tree", required=True,
-                        help="Rooted tree file.")
+requiredArgs.add_argument("-f", "--fasta", dest="file_in", required=True,
+					help="Fasta files.")
+
+requiredArgs.add_argument("-t", "--tree", dest="tree", required=True,
+					help="Rooted tree file.")
 
 parser.add_argument("-F", "--format", dest="format", required=False, default="nexus",
-                        help="Tree file format (newick, nexus, nexml, phyloxml or cdao), default='nexus'.")
+					help="Tree file format (newick, nexus, nexml, phyloxml or cdao), default='nexus'.")
 
-parser.add_argument("-o", "--output", dest="file_out", required=True,
-                    help="The output fasta file name.")
+requiredArgs.add_argument("-o", "--output", dest="file_out", required=True,
+					help="The output fasta file name.")
 
 parser.add_argument("-d", "--drawTree", dest="draw", required=False, action="store_true", default=None,
-                    help="If selected, draws the tree in the console while reordering the fasta file.")
+					help="If selected, draws the tree in the console while reordering the fasta file.")
 
 args = parser.parse_args()
 
 print("  Reading tree")
 T = Phylo.read(args.tree, args.format)
 if args.draw is not None:
-    Phylo.draw_ascii(T)
+	Phylo.draw_ascii(T)
 
 print("  Reading fasta")
 fasta = {}
 for line in SeqIO.parse(open(args.file_in), "fasta"):
-    fasta[line.id] = str(line.seq)
+	fasta[line.id] = str(line.seq)
 
 print("  Reordering and writing fasta")
 with open(args.file_out, "w") as outfile:
-    for line in  T.get_terminals():
-        sp = line.name
-        sp = sp.strip("'")
-        print(">" + str(sp) + "\n" + str(fasta[sp]), file=outfile)
+	for line in  T.get_terminals():
+		sp = line.name
+		sp = sp.strip("'")
+		print(">" + str(sp) + "\n" + str(fasta[sp]), file=outfile)
 
 print("Done")
