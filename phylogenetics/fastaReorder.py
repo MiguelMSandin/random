93511dc683ae550a830c3c8a03bde3b0d4ca7b56
MiguelMSandin <miguelmendezsandin@gmail.com>
2022-03-10 13:38:49 +0100

    Add files via upload

--- phylogenetics/fastaReorder.py
+++ phylogenetics/fastaReorder.py
@@ -2,20 +2,21 @@
 
 import argparse
 from Bio import SeqIO, Phylo
+import re
 
-parser = argparse.ArgumentParser(description="Reorders a fasta file based on a rooted tree.")
+parser = argparse.ArgumentParser(description="Reorders a fasta file based on a tree.")
 
 # Add the arguments to the parser
 requiredArgs = parser.add_argument_group('required arguments')
 
 requiredArgs.add_argument("-f", "--fasta", dest="file_in", required=True,
-					help="Fasta files.")
+					help="Fasta file.")
 
 requiredArgs.add_argument("-t", "--tree", dest="tree", required=True,
-					help="Rooted tree file.")
+					help="Tree file.")
 
-requiredArgs.add_argument("-o", "--output", dest="file_out", required=True,
-					help="The output fasta file name.")
+parser.add_argument("-o", "--output", dest="file_out", required=False, default=None,
+					help="The output fasta file name. By default will add '_treeOrder' to the fasta file name.")
 
 parser.add_argument("-F", "--format", dest="format", required=False, default="newick",
 					help="Tree file format (newick, nexus, nexml, phyloxml or cdao), default='newick'.")
@@ -23,23 +24,39 @@ parser.add_argument("-F", "--format", dest="format", required=False, default="ne
 parser.add_argument("-d", "--drawTree", dest="draw", required=False, action="store_true", default=None,
 					help="If selected, draws the tree in the console while reordering the fasta file.")
 
+parser.add_argument("-v", "--verbose", dest="verbose", required=False, action="store_false",
+					help="If selected, will not print information to the console.")
+
 args = parser.parse_args()
 
-print("  Reading tree")
+# Output file
+if args.file_out is None:
+	outFile = re.sub("\\.[^\\.]+$", "_treeOrder.", args.file_in) + re.sub(".*\\.", "", args.file_in)
+else:
+	outFile = args.file_out
+
+# Reading tree file
+if args.verbose:
+	print("  Reading tree file")
 T = Phylo.read(args.tree, args.format)
 if args.draw is not None:
 	Phylo.draw_ascii(T)
 
-print("  Reading fasta")
+# Reading fasta file
+if args.verbose:
+	print("  Reading fasta file")
 fasta = {}
 for line in SeqIO.parse(open(args.file_in), "fasta"):
 	fasta[line.id] = str(line.seq)
 
-print("  Reordering and writing fasta")
-with open(args.file_out, "w") as outfile:
+# Reordering and exporting
+if args.verbose:
+	print("  Reordering and writing fasta file to:", outFile)
+with open(outFile, "w") as outfile:
 	for line in  T.get_terminals():
 		sp = line.name
 		sp = sp.strip("'")
 		print(">" + str(sp) + "\n" + str(fasta[sp]), file=outfile)
 
-print("Done")
+if args.verbose:
+	print("Done")
