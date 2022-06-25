8850a0f33e64b83b35fe0a0d77eb2c2b35d55435
MiguelMSandin <miguelmendezsandin@gmail.com>
2022-06-25 16:53:08 +0200

    Add files via upload

--- phylogenetics/treeRarefy.py
+++ phylogenetics/treeRarefy.py
@@ -5,33 +5,42 @@ from Bio import SeqIO, Phylo
 import statistics as st
 import random
 
-parser = argparse.ArgumentParser(description="From a tree file, will export a table of branch lengths (from tip to root) from pruned trees after rarefying the tips. If an abundance table is given, abundances will be taken into consideration.")
+parser = argparse.ArgumentParser(description="From a tree file, will export a table of selected distances from rarefied tree tips. If an abundance table is given, abundances will be taken into consideration.")
 
 requiredArgs = parser.add_argument_group('required arguments')
 
 requiredArgs.add_argument("-t", "--tree", dest="tree", required=True,
                     help="A fasta file.")
 
-parser.add_argument("-f", "--format", dest="formaTree", required=False, default='newick',
+parser.add_argument("-f", "--format", dest="formaTree", required=False,
+					default='newick',
 					help="The tree file format: accepted formats are: newick (default), nexus, nexml, phyloxml or cdao.")
 
 parser.add_argument("-a", "--abundance", dest="abundance", required=False, default=None,
-                    help="If selected, will accomodate sequence abundance for the rarification. Then a tab separated table will be needed with two columns: the name of the sequence and the abundance.")
+                    help="If selected, will accomodate sequence abundance for the rarification. Then a tab separated table is needed with two columns: the name of the sequence and the abundance.")
+
+parser.add_argument("-d", "--distance", dest="distance", required=False,
+					default="root",
+					choices=['root', 'branch', 'tips'],
+                    help="The distance to be computed, either: 'root' (default) will calculate the total length from tip to root for every tip, 'branch' will calculate the branch length of every terminal tip, 'tips' will count the number of unique tips")
 
 parser.add_argument("-o", "--output", dest="output", required=False, default=None,
-                    help="The output name of the rarefied table. By default will add '_rarefied.tsv' to file name excluding the extension.")
+                    help="The output name of the rarefied table. By default will add '_rarefiedDistance.tsv' to file name excluding the extension, being 'Distance' the chosen distance.")
 
-parser.add_argument("-r", "--range", dest="iterations", required=False, type=int, nargs=2, default=[0, 20000],
+parser.add_argument("-r", "--range", dest="iterations", required=False, type=int, nargs=2,
+					default=[0, 20000],
                     help="The range of iterations. Will take the minimum and maximum values and create a range of 'steps' for random sampling. Default: '0 20000'.")
 
-parser.add_argument("-s", "--steps", dest="steps", required=False, type=int, default="100",
+parser.add_argument("-s", "--steps", dest="steps", required=False, type=int,
+					default="100",
                     help="The increase of sampling steps. Default: '100'.")
 
-parser.add_argument("-n", "--replicates", dest="replicates", required=False, type=int, default="100",
+parser.add_argument("-n", "--replicates", dest="replicates", required=False, type=int,
+					default="100",
                     help="The number of rarefaction replicates. Default: '100'.")
 
-parser.add_argument("-N", "--normalize", dest="normalize", required=False, action="store_true",
-                    help="If selected, the total branch length will be normalized by number of rarefied tips.")
+parser.add_argument("-m", "--normalize", dest="normalize", required=False, action="store_true",
+                    help="If selected, the distance will be averaged by number of rarefied tips. This will have no effect for distance='tips'.")
 
 parser.add_argument("-R", "--replacement", dest="replacement", required=False, action="store_true",
                     help="If selected, the random sampling will be done without replacement.")
@@ -46,8 +55,20 @@ args = parser.parse_args()
 
 # Setting parameters _______________________________________________________________________________
 if args.output is None:
+	if args.distance == "root":
+		ext = "Root"
+	elif args.distance == "branch":
+		ext = "Branch"
+	elif args.distance == "tips":
+		ext = "Tips"
+	else:
+		ext = ""
+	if args.normalize:
+		n = "N"
+	else:
+		n = ""
 	import re
-	outFile = re.sub("\\.[^\\.]+$", "_rarefied.tsv", args.tree)
+	outFile = re.sub("\\.[^\\.]+$", "_rarefied", args.tree) + ext + n + ".tsv"
 else:
 	outFile = args.output
 
@@ -63,21 +84,43 @@ if args.verbose:
 T = Phylo.read(args.tree, args.formaTree)
 tipsN = T.count_terminals()
 
-branchLengths = {}
-if args.verbose:
-	print("  Getting branch legths to root", end="")
-	i = 0
-	P = 0
-for tip in T.get_terminals():
+if args.distance != "tips":
+	distances = {}
 	if args.verbose:
-			i += 1
-			I = round(i/tipsN*100)
-			if I > P:
-				P = I
-				print("\r  Getting branch legths to root ", P, "%", sep="", end="")
-	branchLengths[tip.name] = T.distance(tip)
+		if args.distance == "root":
+			print("  Getting branch legths to root", end="")
+		if args.distance == "branch":
+			print("  Getting branch legths", end="")
+		i = 0
+		P = 0
+	for tip in T.get_terminals():
+		if args.verbose:
+				i += 1
+				I = round(i/tipsN*100)
+				if I > P:
+					P = I
+					if args.distance == "root":
+						print("\r  Getting branch legths to root ", P, "%", sep="", end="")
+					if args.distance == "branch":
+						print("\r  Getting branch legths ", P, "%", sep="", end="")
+		if args.distance == "root":
+			distances[tip.name] = T.distance(tip)
+		if args.distance == "branch":
+			distances[tip.name] = tip.branch_length
+	if args.verbose:
+		print("")
+
 if args.verbose:
-	print("")
+	if args.distance == "root" or args.distance == "branch":
+		length = list()
+		for l in distances.values():
+			length.append(l)
+	if args.distance == "root":
+		print("  The tree has", tipsN, "tips,", round(sum(length), 2), "total branch length from tip to root and", round(st.mean(length), 2), "on average.")
+	if args.distance == "branch":
+		print("  The tree has", tipsN, "tips,", round(sum(length), 2), "total branch lengths and", round(st.mean(length), 2), "on average.")
+	if args.distance == "tips":
+		print("  The tree has", tipsN, "tips")
 
 # Reading abundances _______________________________________________________________________________
 if args.abundance is not None:
@@ -133,7 +176,7 @@ if args.verbose:
 		"\n      -with", args.replicates, "replicates\n      -in the total",
 		len(reads), "tips")
 
-# Test if it is possible to do not use replacement if selected
+# If selected replacement option, test if it is possible to use
 if args.replacement:
 	if max(steps) > len(reads):
 		print("\nWarning! You have selected a maximum sampling of", max(steps),
@@ -148,10 +191,7 @@ if args.verbose:
 	i = 0
 	P = 0
 with open(outFile, 'w') as outfile:
-	if args.normalize:
-		outfile.write("sampleSize\tmeanNorm\tsdNorm\tmin\tp05\tp25\tp50\tp75\tp95\tmax\n")
-	else:
-		outfile.write("sampleSize\tmeanTotal\tsdTotal\tmin\tp05\tp25\tp50\tp75\tp95\tmax\n")
+	outfile.write("sampleSize\tmean\tsd\tmin\tp05\tp25\tp50\tp75\tp95\tmax\n")
 	for s in steps:
 		if args.verbose:
 			i += 1
@@ -167,11 +207,18 @@ with open(outFile, 'w') as outfile:
 				rarefied = set(random.sample(reads, k=s))
 			else:
 				rarefied = set(random.choices(reads, k=s))
-			for tip in rarefied:
-				lengths.append(branchLengths[tip])
-			lengthOut = sum(lengths)
-			if args.normalize:
-				lengthOut = lengthOut/s
+			if args.distance == "tips":
+				lengthOut = len(rarefied)
+			else:
+				for tip in rarefied:
+					lengths.append(distances[tip])
+				if args.normalize:
+					if len(lengths) == 0:
+						lengthOut = 0
+					else:
+						lengthOut = st.mean(lengths)
+				else:
+					lengthOut = sum(lengths)
 			sample.append(lengthOut)
 		sort = sorted(sample)
 		line = str(str(s) + '\t' +
@@ -188,47 +235,6 @@ with open(outFile, 'w') as outfile:
 
 if args.verbose:
 	print("\n  Table exported to:", outFile)
-
-if args.printSummary:
-	print("  Final summary report:")
-	total = 0
-	branchLengths
-	for l in branchLengths.values():
-		total += l
-	print("    Tree has a total of", tipsN, "tips and", round(total, 2), "total branch length")
-	sample = list()
-	for j in range(0, args.replicates):
-		random.seed()
-		if args.replacement:
-			rarefied = set(random.sample(reads, k=tipsN))
-		else:
-			rarefied = set(random.choices(reads, k=tipsN))
-		lengths = list()
-		for tip in rarefied:
-			lengths.append(branchLengths[tip])
-		lengthOut = sum(lengths)
-		if args.normalize:
-			lengthOut = lengthOut/s
-		sample.append(lengthOut)
-	tmp = st.mean(sample)
-	print("\r    When sampling ", tipsN, " tips, an average branch length of ", round(tmp, 2), " is retrieved (", round(tmp/total*100, 2), "%)", sep="")
-	if args.abundance is not None:
-		sample = list()
-		for j in range(0, args.replicates):
-			random.seed()
-			if args.replacement:
-				rarefied = set(random.sample(reads, k=len(reads)))
-			else:
-				rarefied = set(random.choices(reads, k=len(reads)))
-			lengths = list()
-			for tip in rarefied:
-				lengths.append(branchLengths[tip])
-			lengthOut = sum(lengths)
-			if args.normalize:
-				lengthOut = lengthOut/s
-			sample.append(lengthOut)
-		tmp = st.mean(sample)
-		print("\r    When sampling ", len(reads), " tips, an average branch length of ", round(tmp, 2), " is retrieved (", round(tmp/total*100, 2), "%)", sep="")
 
 # __________________________________________________________________________________________________
 if args.verbose:
