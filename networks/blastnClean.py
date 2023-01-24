db8bf2cb17c65532a1268e2273145c2a008c53f7
MiguelMSandin <miguelmendezsandin@gmail.com>
2023-01-24 18:05:09 +0100

    Add files via upload

---
+++ networks/blastnClean.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Removes reciprocal hits (A-B = B-A).")
+
+# Add the arguments to the parser
+requiredArgs = parser.add_argument_group('required arguments')
+
+requiredArgs.add_argument("-f", "--file", dest="file_in", required=True,
+                    help="Input file. This assumes a file with the following columns: 'qseqid sseqid evalue pident bitscore qstart qend qlen sstart send slen'.")
+
+parser.add_argument("-o", "--output", dest="file_out", required=False, default=None,
+                    help="Output file. Returns the filtered file to the specified location. By default will add '_clean.net' to the input file, after removing the extension.")
+
+args = parser.parse_args()
+
+if args.file_out is None:
+	out = re.sub("\\.[^\\.]+$", "_clean.net", args.file_in)
+else:
+	out = args.out
+
+print("Cleaning")
+
+clean = {}
+hits = set()
+lines = 0
+removed = 0
+selfHit = 0
+accepted = 0
+with open(out, "w") as outfile:
+	for line in open(args.file_in):
+		lines += 1
+		line = line.strip().split()
+		seq1 = line[0]
+		seq2 = line[1]
+		if seq1 == seq2:
+			selfHit += 1
+		hit = str(seq1) + "\t" + str(seq2)
+		hitr =str(seq2) + "\t" + str(seq1)
+		if hit in clean or hitr in clean:
+			removed += 1
+		else:
+			accepted += 1
+			hits.add(hit)
+			hits.add(hitr)
+			values = str(line[2]) + "\t" + str(line[3]) + "\t" + str(line[4]) + "\t" + str(line[5]) + "\t" + str(line[6]) + "\t" + str(line[7]) + "\t" + str(line[8]) + "\t" + str(line[9]) + "\t" + str(line[10])
+			clean[hit] = values
+			print(hit + "\t" + clean[hit], file=outfile)
+
+print("  Hits in:      ", lines)
+print("  Hits removed: ", removed)
+print("  Self hits:    ", selfHit)
+print("  Hits out:     ", accepted)
+print("Done")
+
---
+++ networks/netCCextract.py
@@ -0,0 +1,84 @@
+#! /usr/local/bin/python3.7
+
+import argparse
+import networkx as nx
+import re
+
+parser = argparse.ArgumentParser(description="Extracts attributes of nodes present in the network to speed processing.")
+
+# Add the arguments to the parser
+requiredArgs = parser.add_argument_group('required arguments')
+
+requiredArgs.add_argument("-f", "--file", dest="file_in", required=True,
+                    help="Net file. A file with three columns, origin node, destination node and identity value.")
+
+requiredArgs.add_argument("-a", "--attributes", dest="attributes", required=True,
+                    help="A file with the nodes and the attributes of each node.")
+
+requiredArgs.add_argument("-g", "--group", dest="group", required=True,
+                    help="The name of the attribute you want to keep.")
+
+parser.add_argument("-o", "--output", dest="file_out", required=False, default=None,
+                    help="Output file. By default will add '_subset.net' to input file.")
+
+args = parser.parse_args()
+
+if args.file_out is None:
+	out = re.sub("\\.[^\\.]+$", "_subset.net", args.file_in)
+else:
+	out = args.out
+
+print("\n  Reading network")
+G=nx.read_edgelist(args.file_in, delimiter="\t", data=(("id",float),))
+
+nodes=list(G.nodes())
+
+print("  Reading attributes file", end="")
+attr = {}
+for line in open(args.attributes):
+    line = line[:-1].split("\t")
+    key = line[0]
+    key = re.sub(" $", "", key)
+    value = line[1]
+    value = re.sub("^ ", "", value)
+    attr[key] = value
+
+print(" and assigning attributes to network")
+attributes = {}
+for node in nodes:
+    attributes[node] = attr.get(node)
+
+nx.set_node_attributes(G, attributes, "attributes")
+
+print("  Cleaning network")
+c = 0
+k = 0
+n = 0
+toRemove = set()
+count = nx.number_connected_components(G)
+CCs = (G.subgraph(CCs) for CCs in nx.connected_components(G))
+for CC in CCs:
+    c += 1
+    print("\r    Working on CC ", c, "/", count, sep="", end="")
+    groups = nx.get_node_attributes(CC, "attributes")
+    groups = groups.values()
+    groups = set(groups)
+    if args.group not in groups:
+        k += 1
+        for node in CC:
+            n += 1
+            toRemove.add(node)
+            
+print("\n  Removing:")
+print("      ", k, " CCs", sep="")
+print("      ", n, " nodes", sep="")
+
+G.remove_nodes_from(toRemove)
+
+print("    Initial network had  ", len(nodes), " nodes and ", count, " CCs", sep="")
+print("    Exported network has ", len(list(G.nodes())), " nodes and ", nx.number_connected_components(G), " CCs", sep="")
+
+print("  Writing network with")
+nx.write_edgelist(G, out, delimiter="\t", data=["id"])
+
+print("\nDone\n")
