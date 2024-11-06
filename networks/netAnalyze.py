e153583c0c5d9652d01b21330b401ad36a032699
MiguelMSandin <miguelmendezsandin@gmail.com>
2024-11-06 10:04:16 +0100

    Update netAnalyze.py

--- networks/netAnalyze.py
+++ networks/netAnalyze.py
@@ -26,7 +26,7 @@ else:
 outNetwork = out + "_network.net"
 outNodes = out + "_nodes.net"
 
-print("\n  Reading network")
+print("\n  Reading network", flush=True)
 G=nx.read_edgelist(args.file_in, delimiter="\t", data=(("id",float),))
 
 nodes=list(G.nodes())
@@ -41,58 +41,58 @@ for CC in CCs:
 
 print("\n  Calculating properties of the network")
 with open(outNetwork, "w") as outfile:
-    print(("Input file: " + "\t" + str(args.file_in)), file=outfile)
+    print(("Input file: " + "\t" + str(args.file_in)), file=outfile, flush=True)
     # Number of nodes
-    print("    Number of nodes", end="")
-    print(("Number of nodes" + "\t" + str(len(nodes))), file=outfile)
+    print("    Number of nodes", end="", flush=True)
+    print(("Number of nodes" + "\t" + str(len(nodes))), file=outfile, flush=True)
     # Number of edges
-    print(": done \n    Number of edges", end="")
-    print(("Number of edges" + "\t" + str(len(edges))), file=outfile)
+    print(": done \n    Number of edges", end="", flush=True)
+    print(("Number of edges" + "\t" + str(len(edges))), file=outfile, flush=True)
     # Average number of neighbours 
-    print(": done \n    Connectivity", end="")
+    print(": done \n    Connectivity", end="", flush=True)
     tmp = dict(nx.degree(G))
     tmp = tmp.values()
     tmp = st.mean(tmp)  
-    print(("Connectivity" + "\t" + str(tmp)), file=outfile)    
+    print(("Connectivity" + "\t" + str(tmp)), file=outfile, flush=True)
     #print(("Connectivity" + "\t" + str(nx.average_degree_connectivity(G))), file=outfile)
     # Clustering coefficient: E/((N(N-1))/2)
-    print(": done \n    Clustering coefficient", end="")
-    print(("Clustering coefficient" + "\t" + str(nx.density(G))), file=outfile)
+    print(": done \n    Clustering coefficient", end="", flush=True)
+    print(("Clustering coefficient" + "\t" + str(nx.density(G))), file=outfile, flush=True)
     # Connected components
-    print(": done \n    Connected components", end="")
-    print(("Connected components" + "\t" + str(count)), file=outfile)
-    print(": done\n")
+    print(": done \n    Connected components", end="", flush=True)
+    print(("Connected components" + "\t" + str(count)), file=outfile, flush=True)
+    print(": done\n", flush=True)
 
 if count > 1:
-    print("  There are '", count, "' connected components (CC). Analyzing each one of them individually as an independent network:", sep="")
+    print("  There are '", count, "' connected components (CC). Analyzing each one of them individually as an independent network:", sep="", flush=True)
     outNetworkCCs = out + "_network_CCs"
     #CCs = nx.connected_component_subgraphs(G)  # When using a 'networkx' version below 2.1
     CCs = (G.subgraph(CCs) for CCs in nx.connected_components(G))
     c = 0
     with open(outNetworkCCs, "w") as outfile:
-        print("Connected_component \t Nnodes \t Nedges \t Connectivity \t Density", file=outfile)  
+        print("Connected_component \t Nnodes \t Nedges \t Connectivity \t Density", file=outfile, flush=True)
         for CC in CCs:
             c = c +1
-            print("\r    Working on CC ", c, "/", count, sep="", end="")
+            print("\r    Working on CC ", c, "/", count, sep="", end="", flush=True)
             nnodes = len(list(CC.nodes()))
             nedges = len(list(CC.edges()))
             tmp = dict(nx.degree(CC))
             tmp = tmp.values()
             connec = st.mean(tmp)
             densit = nx.density(CC)          
-            print((str(c) + "\t" + str(nnodes) + "\t" + str(nedges) + "\t" + str(connec) + "\t" + str(densit)), file=outfile)
-        print("\n")
+            print((str(c) + "\t" + str(nnodes) + "\t" + str(nedges) + "\t" + str(connec) + "\t" + str(densit)), file=outfile, flush=True)
+        print("\n", flush=True)
 
-print("  Calculating properties of the nodes")
+print("  Calculating properties of the nodes", flush=True)
 #CCs = nx.connected_component_subgraphs(G)  # When using a 'networkx' version below 2.1
 CCs = (G.subgraph(CCs) for CCs in nx.connected_components(G))
 with open(outNodes, "w") as outfile:
-    print("Connected_component \t Node \t Degree \t Betweenness \t Closeness \t Eccentricity", file=outfile)
+    print("Connected_component \t Node \t Degree \t Betweenness \t Closeness \t Eccentricity", file=outfile, flush=True)
     c = 0
     for CC in CCs:
         c = c + 1
         if count > 1:
-            print("\r    Working on CC ", c, "/", count, sep="", end="")            
+            print("\r    Working on CC ", c, "/", count, sep="", end="", flush=True)
         # Degree: Number of edges related to a node
         degree = nx.degree(CC)
         # Betweenness: Frequency of being in a shortest path
@@ -102,5 +102,5 @@ with open(outNodes, "w") as outfile:
         # Eccentricity: Average longest distance between a node and all other nodes
         eccentricity = nx.eccentricity(CC)
         for node in CC.nodes():
-            print((str(c) + "\t" + str(node) + "\t" + str(degree[node]) + "\t" + str(betweeness[node]) + "\t" + str(closeness[node]) + "\t" + str(eccentricity[node])), file=outfile)
-    print("\nDone")
+            print((str(c) + "\t" + str(node) + "\t" + str(degree[node]) + "\t" + str(betweeness[node]) + "\t" + str(closeness[node]) + "\t" + str(eccentricity[node])), file=outfile, flush=True)
+    print("\nDone", flush=True)
